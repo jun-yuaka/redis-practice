897f85fbc6ad76561e5f26913e71985c89c8dc13
jun-yuaka <yuaka0511@gmail.com>
2023-07-23 08:43:37 +0900

    valueの保存

--- app/main.go
+++ app/main.go
@@ -16,10 +16,19 @@ func main() {
 	})
 
 	ctx := context.Background()
+
+	// Set a key
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		fmt.Println("Set error", err)
+	} else {
+		fmt.Println("Key set successfully.")
+	}
+
 	for {
 		val, err := rdb.Get(ctx, "key").Result()
 		if err != nil {
-			fmt.Println("error:", err)
+			fmt.Println("Get error:", err)
 		} else {
 			fmt.Println("key", val)
 		}
